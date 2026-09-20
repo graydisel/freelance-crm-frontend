7e2624f94aae55391c6c4ee1dba6c317a1d0f3e9
Sergii Babanenkov <graydisel@gmail.com>
2026-09-20 13:54:22 +0300

    Update Login Page

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,15 +1,6 @@
-<<<<<<< Updated upstream
 export const environment = {
   production: true,
   apiUrl: 'https://freelance-crm-backend-wr97.onrender.com',
   token: 'crm_token',
-  userKey: 'crm_user'
+  userKey: 'crm_user',
 };
-=======
-export const environment = {
-  production: true,
-  apiUrl: 'https://freelance-crm-backend-wr97.onrender.com',
-  token: 'crm_token',
-  userKey: 'crm_user'
-};
->>>>>>> Stashed changes
