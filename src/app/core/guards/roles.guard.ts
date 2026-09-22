86acb3845ec39085d36541d445f2987cf010c2a3
Sergii Babanenkov <graydisel@gmail.com>
2026-09-22 16:37:40 +0300

    Changed guards by Role Guard. Add forbidden page.

---
+++ src/app/core/guards/roles.guard.ts
@@ -0,0 +1,23 @@
+import { CanActivateFn, Router, ActivatedRouteSnapshot, RouterStateSnapshot } from "@angular/router";
+import { inject } from '@angular/core';
+import { AuthStore } from "../stores/auth.store";
+
+export const rolesGuard: CanActivateFn = (route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => {
+    const authStore = inject(AuthStore);
+    const router = inject(Router);
+
+    const requiredRoles = route.data['roles'] as Array<string>;
+    if (!requiredRoles || requiredRoles.length === 0) {
+        return true;
+    }
+
+    const role = authStore.role();
+    if (!role) return false;
+
+    const hasAccess = requiredRoles.includes(role.toLowerCase());
+    if (hasAccess) {
+        return true;
+    } else {
+        return router.createUrlTree(['/forbidden']);
+    }
+}
